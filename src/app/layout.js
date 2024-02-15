96757ba7e94188b3e2b5908c2ce5aa1c2ee20c2d
Zach Zimbler <zachzimbler@gmail.com>
2024-02-15 11:29:58 -0700

    Updated small branding things

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,10 @@
 import './globals.css'
 import { SwishjamProvider } from '@swishjam/react'
+import { Toaster } from "@/components/ui/sonner"
 
 export const metadata = {
-  title: 'Prompt Craft',
-  description: 'AI Chatbot Prompt Builder for your amusement',
+  title: 'Promptcraft',
+  description: 'An Open-Source Hackable AI Chatbot Prompt Builder',
   openGraph: {
     title: 'Prompt Craft', 
     images: [{
@@ -16,13 +17,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <SwishjamProvider apiKey=''>
+    <SwishjamProvider apiKey='swishjam-1b73e1bd-c8b19919'>
       <html lang="en">
         <head>
           <meta name="theme-color" content="#2e5e38" />
           <meta name="robots" content="noindex" />
         </head>
         <body className="bg-gray-900">{children}</body>
+        <Toaster richColors />
       </html>
     </SwishjamProvider>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 'use client'
 
-//import { toast } from 'sonner'
+import { toast } from 'sonner'
 //import { swishjam } from '@swishjam/react'
 //import { Button } from "@/components/ui/button"
+import Logo from '@/components/logo'
 import React, { useEffect, useState } from 'react'
 import { motion } from "framer-motion"
 import { Input } from "@/components/ui/input"
@@ -15,7 +16,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
-import { LuCopy } from 'react-icons/lu'
+import { LuCopy, LuArrowUpRight } from 'react-icons/lu'
 import { ComboboxPopover } from '@/components/combobox'
 import { useForm } from "react-hook-form"
 import { useQueryState } from 'nuqs'
@@ -47,27 +48,27 @@ export default function Home() {
   }})
  
 
-  // form.watch((data, {}) => {
-  //   console.log('logging data from form watch:') 
-  //   console.log(data)
-  //   const promptText = `${data.role ? 'Act like a '+data.role+',' : ''}
-  //     ${data.needs ? 'I need a '+data.needs+',' : ''}
-  //     ${data.tasks ? 'you will '+data.tasks+',' : ''}
-  //     ${data.process ? 'in the process, you should '+data.process+',' : ''}
-  //     ${data.exclusions ? 'please '+data.exlusions+',' : ''}
-  //     ${data.format ? 'output the final result in '+data.format+',' : ''}
-  //     ${data.example ? 'here is an example: '+data.example+',' : ''}`;
+  form.watch((data, {}) => {
+    console.log('logging data from form watch:') 
+    console.log(data)
+    const promptText = `${data.role ? 'Act like a '+data.role+',' : ''}
+      ${data.needs ? 'I need a '+data.needs+',' : ''}
+      ${data.tasks ? 'you will '+data.tasks+',' : ''}
+      ${data.process ? 'in the process, you should '+data.process+',' : ''}
+      ${data.exclusions ? 'please '+data.exlusions+',' : ''}
+      ${data.format ? 'output the final result in '+data.format+',' : ''}
+      ${data.example ? 'here is an example: '+data.example+',' : ''}`;
   
-  //   setFullStr(promptText)
-  // })
+    setFullStr(promptText)
+  })
 
   return (
-    <div>
+    <div className=''>
       <main className={`min-h-screen p-8 overflow-hidden dark mx-auto max-w-7xl grid grid-cols-2 gap-x-8 content-start`}>
-        <div className='col-span-2'>
-          <h2 className="text-base font-semibold leading-7 text-white">Prompt Craft</h2>
+        <div className='col-span-2 mb-4'>
+          <h1 className="text-base font-semibold leading-7 text-white">Promptcraft</h1>
           <p className="mt-1 text-sm leading-6 text-gray-400">
-            AI Chatbot Prompt Builder for your amusement
+            An Open-Source Hackable AI Chatbot Prompt Builder <a href="https://github.com" className='flex items-center duration-300 transition-all cursor-pointer hover:text-emerald-500'>view on Github <LuArrowUpRight size={16} className='ml-1'/></a>
           </p>
         </div>
         <motion.div
@@ -171,7 +172,7 @@ export default function Home() {
                     <FormControl className="col-span-8">
                       <Textarea
                         className="text-white"
-                        placeholder="Avoid keyword stuff"
+                        placeholder="Avoid keyword stuffing"
                         autoComplete="off"
                         {...form.register("exclusions")}
                       />
@@ -210,7 +211,7 @@ export default function Home() {
                 render={({ field }) => (
                   <FormItem className="grid grid-cols-8 gap-x-2">
                     <FormLabel className="text-white col-span-8">Here is an example</FormLabel>
-                    <FormControl className="col-span-7">
+                    <FormControl className="col-span-8">
                       <Textarea
                         className="text-white"
                         type="text"
@@ -219,11 +220,6 @@ export default function Home() {
                         {...form.register("example")}
                       />
                     </FormControl>
-                    <ComboboxPopover
-                      className="col-span-1"
-                      presets={rolePresets}
-                      onSelect={(v) => {form.setValue('example', v)}} 
-                    />
                     <FormMessage className="col-span-8" />
                   </FormItem>
                 )}
@@ -244,6 +240,7 @@ export default function Home() {
             {fullStr}
             <CopyToClipboard
               text={fullStr}
+              onCopy={() => toast.success('Copied to clipboard')}
             >
               <div className='cursor-pointer p-2 rounded-md bg-white/5 hover:bg-white/20 duration-500 transition-all absolute top-2 right-2'>
                 <LuCopy className="w-4 h-4 text-white" />
@@ -252,6 +249,15 @@ export default function Home() {
           </div>
         </motion.div>
       </main>
+      <div className='absolute bottom-8 right-8'>
+        <p className='text-white'>Built by <a href="https://twitter.com/zzimbler" className="duration-300 transition-all hover:text-emerald-500 cursor-pointer">@zzimbler</a></p> 
+        <a href="https://swishjam.com?utm_campaign=promptcraft&utm_source=promptcraft&ref=promptcraft" className='block border-4 border-white p-4 rounded-md mt-4 bg-white cursor-pointer duration-300 transition-all hover:border-emerald-500 hover:border-4'>
+          <p>Sponsored by</p>
+          <div className='h-8'>
+            <Logo />
+          </div>
+        </a>
+      </div>
     </div>
   )
 }
